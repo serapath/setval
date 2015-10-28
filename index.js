c4bb40d699c98d631d916bdb70f6b268ecb821f7
serapath (Alexander Praetorius) <dev@serapath.de>
2015-10-29 00:27:18 +0100

    FIX overwrite sibling properties

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var isobject = require('isobject')
 module.exports = function setval (root, path, value) {
   var t = root, keys = (''+path).split('.'), last = keys.pop()
   keys.forEach(function(key) {
-    t = t[key] = t[key] !== isobject(t[key]) ? {} : t[key]
+    t = t[key] = isobject(t[key]) ? {} : t[key]
   })
   t[last] = value
 }
