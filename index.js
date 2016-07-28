080e0c7670c63d8341c58c59914ae18ec98c22c6
serapath <dev@serapath.de>
2016-07-28 20:04:12 +0200

    ADD delimiter; UPDATE default delimiter to '/'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var isobject = require('isobject')
 module.exports = function setval (root, path, value) {
-  var t = root, keys = (''+path).split('.'), last = keys.pop()
+  var t = root, keys = (''+path).split(delimiter||'/'), last = keys.pop()
   keys.forEach(function(key) {
     t = t[key] = isobject(t[key]) ? t[key] : {}
   })
