c2a832d31ca4d5ee0cea283903bc53aa17d8727a
serapath <dev@serapath.de>
2016-07-29 00:47:11 +0200

    FIX delimiter param bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var isobject = require('isobject')
-module.exports = function setval (root, path, value) {
+module.exports = function setval (root, path, value, delimiter) {
   var t = root, keys = (''+path).split(delimiter||'/'), last = keys.pop()
   keys.forEach(function(key) {
     t = t[key] = isobject(t[key]) ? t[key] : {}
